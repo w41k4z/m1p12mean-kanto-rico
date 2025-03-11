8ba38f2e4dec0e7f978ba96f1cf44b2ebf73e9cf
Kanto Andrianotahina <118660424+Kanto-Andrianotahina@users.noreply.github.com>
2025-03-11 13:39:08 +0300

    [Dev][Metier] -  adding functionalities CRUD service

--- server/src/controllers/service.controller.js
+++ server/src/controllers/service.controller.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const createService = require('../services/service.factory.service').createService;
+const getAllServices = require('../services/service.factory.service').getAllServices;
+const updateService = require('../services/service.factory.service').updateService;
+const deleteService = require('../services/service.factory.service').deleteService;
 
 router.post('/createService', async (req, res, next) => {
     try {
@@ -8,6 +11,35 @@ router.post('/createService', async (req, res, next) => {
         });
         await newService.save();
         res.json({ message: 'Service created' });
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/getAllServices', async (req, res, next) => {
+    try {
+        const services = await getAllServices();
+        res.json(services);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.put('/updateService', async (req, res, next) => {
+    try {
+        const updatedService = await updateService(req.body.id, req.body.name);
+        await updatedService.save();
+        res.json({ message: 'Service updated' });
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.delete('/deleteService', async (req, res, next) => {
+    try {
+        const deletedService = await deleteService(req.body.id);
+        await deletedService.remove();
+        res.json({ message: 'Service deleted' });
     } catch (error) {
         next(error);
     }
