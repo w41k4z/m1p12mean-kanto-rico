c7abd5db1d5cccad62cdd6295dcad8a4bb93781d
w41k4z <alainricor@gmail.com>
2025-03-14 15:06:25 +0300

    Api response configuration

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,29 +1,16 @@
-const router = require('express').Router();
-const passport = require('passport');
+const router = require("express").Router();
+const passport = require("passport");
+const ApiResponse = require("../config/response/api.response");
+const jwtService = require("../services/jwt.service");
 
-const generateToken = require('../services/jwt.service').generateToken;
-const createUser = require('../services/user.factory.service').createUser;
-const Roles = require('../config/roles');
-
-router.post('/sign-in', passport.authenticate('local', { session: false }), (req, res) => {
-    const token = generateToken(req.user);
-    res.json({ accessToken: token });
-})
-
-router.post('/sign-up', async (req, res, next) => {
-    try {        
-        const newUser = await createUser({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            username: req.body.username,
-            password: req.body.password,
-            roleName: Roles.CLIENT
-        });
-        await newUser.save();
-        res.json({ message: 'Client account created' });
-    } catch (error) {
-        next(error);
-    }
-});
+router.post(
+  "/sign-in",
+  passport.authenticate("local", { session: false }),
+  (req, res) => {
+    const token = jwtService.generateToken(req.user);
+    const payload = { accessToken: token };
+    res.json(new ApiResponse(payload));
+  }
+);
 
 module.exports = router;
