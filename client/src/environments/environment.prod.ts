a0c2ba8be9ad7d38dc6236e6728c5d001c9f9edb
w41k4z <alainricor@gmail.com>
2025-03-29 14:54:02 +0300

    Fixed client server base url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const env = {
-    baseUrl: 'https://m1p12mean-kanto-rico-production.up.railway.app/',
+    baseUrl: 'https://m1p12mean-kanto-rico-production.up.railway.app',
     production: true,
 };
